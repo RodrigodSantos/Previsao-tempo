9a8d6a2897ba1b38b7f2444c5954c9a346a9c6a2
Rodrigo dos Santos <94724162+RodrigodSantos@users.noreply.github.com>
2025-01-07 21:47:38 -0300

    1 part

---
+++ src/App.jsx
@@ -0,0 +1,33 @@
+import { useState, useRef } from 'react'
+import axios from 'axios'
+import './App.css'
+
+import WeatherInfo from './components/WeatherInfo/WeatherInfo'
+
+function App() {
+  const [weather, setWeather] = useState()
+  const inputRef = useRef()
+  
+
+  async function searchCity() {
+    const city = inputRef.current.value
+    const key = "b1aaecb33703e4f534c10467864eac82"
+
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}&lang=pt_br&units=metric`
+
+    const apiInfo = await axios.get(url)
+    setWeather(apiInfo.data)    
+  }
+
+  return (
+    <div className='container'>
+    
+      <h1>Previsão do tempo</h1>
+      <input ref={inputRef} type="text" placeholder='Digite o nome da cidade'/>
+      <button onClick={searchCity}>Buscar</button>
+      {weather && <WeatherInfo weather={weather}/>}
+    </div>
+  )
+}
+
+export default App
